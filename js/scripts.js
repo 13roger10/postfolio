c5e65206dc16bc57b71abb3f60f5c6aec85ae301
ROGERIO FERREIRA MARTINS <rogeriio.martins@gmail.com>
2023-08-08 16:08:08 -0300

    Commit javaScipt
    Adicionado arquivos javaScipt

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const changeThemeBtn = document.querySelector("#change-theme")
+
+//togle dark mode
+function toggleDarkMode(){
+    document.body.classList.toggle("dark")
+}
+//Load light or dark mode
+function loadTheme(){
+    const darkMode = localStorage.getItem("dark");
+    if (darkMode){
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkMode();
+
+    //Save or romove dark mode 
+    localStorage.removeItem("dark");
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark",1);
+    }
+});
+
